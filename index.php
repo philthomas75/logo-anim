ca04ad8077758f89c78044a3f54c4e28c1319295
Phil Thomas <philip.thomas@cognizant.com>
2013-07-18 10:54:46 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="google" content="notranslate">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="Content-Language" content="en" />
+    <title>Experiments</title>
+    <meta name="description" content="">
+    <!-- <meta name="viewport" content="width=device-width"> -->
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="stylesheet" href="style.css">
+</head>
+    
+<body>
+    <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
+    <div class="wrapper">
+        <header>
+            <div class="logo"><h1><a href="http://www.plainresponsive.com">PLAIN RESPONSIVE</a></h1></div>
+<!--             <nav role="navigation">
+                <ul class="header-buttons-list">
+                    <li class="nav-item"><a href="#">One</a></li>
+                    <li class="nav-item"><a href="#">Two</a></li>
+                    <li class="nav-item"><a href="#">Three</a></li>
+                    <li class="nav-item"><a href="#">Four</a></li>
+                    <li class="nav-item"><a href="#">Five</a></li>
+                </ul>
+            </nav>  -->  
+        </header>
+        
+<!--         <div role="main" class="main-content">
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        </div> -->
+
+<!--     <footer role="contentinfo">
+        <div class="inner-footer">
+            <p class="copyright">&copy; Ben Frain, 2013. All rights reserved.</p>
+        </div>
+    </footer> -->
+
+    </div>
+
+</body>
+
+
+
+	       
